67e02e9bd963578925c7f0db41298238fae88348
Adam <ashaw315@gmail.com>
2021-11-22 23:10:32 -0500

    commit tree 4bf77dd83fc74cd81e26fd46dfbe78795bd74c6d

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 15.5
+const num2 = 4
 
-// Write your code here
+const multiply = num1*num2
+
+let random = Math.floor(Math.random() + 1);
+
+const num3 = 12
+const num4 = 3
+
+let mod =(num3/num4);
+
+
+
+let max = Math.max(5, 10, 20);
